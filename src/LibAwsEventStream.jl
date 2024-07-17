7fbe0b79b40d70fadedc842d076ef8554980f92a
Jacob Quinn <quinn.jacobd@gmail.com>
2024-07-16 18:47:18 -0600

    Bump version, deps versions, and add default init (#4)

--- src/LibAwsEventStream.jl
+++ src/LibAwsEventStream.jl
@@ -47,4 +47,13 @@ for name in names(@__MODULE__; all=true)
     @eval export $name
 end
 
+function init(allocator=default_aws_allocator())
+    LibAwsCommon.init(allocator)
+    LibAwsCal.init(allocator)
+    LibAwsChecksums.init(allocator)
+    LibAwsIO.init(allocator)
+    aws_event_stream_library_init(allocator)
+    return
+end
+
 end
