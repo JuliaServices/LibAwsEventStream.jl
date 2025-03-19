5d0abddbff3b7eac1f24aef75d3ec08e56a315f4
Jacob Quinn <quinn.jacobd@gmail.com>
2025-03-18 18:34:44 -0600

    add ref to remove

--- gen/generator.jl
+++ gen/generator.jl
@@ -13,7 +13,7 @@ using LibAwsIO
 
 cd(@__DIR__)
 
-const refs_to_remove = []
+const refs_to_remove = ("INT16_MAX",)
 
 # This is called if the docs generated from the extract_c_comment_style method did not generate any lines.
 # We need to generate at least some docs so that cross-references work with Documenter.jl.
